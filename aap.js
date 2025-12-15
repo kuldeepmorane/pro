e7157f4fa7cb3eca52bae33535c4d7bd8c29c71c
Kuldeep Morane <kuldeepmorane305@gmail.com>
2025-12-15 16:16:29 +0530

    add new form

--- aap.js
+++ aap.js
@@ -1,4 +1,5 @@
 let h1 = document.querySelector("h1");
 h1.addEventListener("click", () => {
-    alert("dont click any more");
-})
+        alert("dont click any more");
+    })
+    //add new form
